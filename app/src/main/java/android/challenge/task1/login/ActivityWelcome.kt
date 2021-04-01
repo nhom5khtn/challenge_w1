0657409474379c4a77895002e043e86f26221c9b
Thuan <votuonganvn@gmail.com>
2021-04-01 23:41:43 +0700

    Init

---
+++ app/src/main/java/android/challenge/task1/login/ActivityWelcome.kt
@@ -0,0 +1,11 @@
+package android.challenge.task1.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityWelcome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
---
+++ app/src/main/java/android/challenge/task1/login/OnboardActivity.kt
@@ -0,0 +1,11 @@
+package android.challenge.task1.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboard)
+    }
+}
